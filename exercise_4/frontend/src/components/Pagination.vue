ab6328b59f070a2ebd223ab1a44343d94f01d907
EversonDoNascimento <everson25nascimento@gmail.com>
2025-03-28 15:01:20 -0300

    feat: Created Pagination component to controller the quantity of response per page

---
+++ exercise_4/frontend/src/components/Pagination.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="pagination">
+    <span v-for="(page, index) in pages" key="index">
+      <a
+        @click.prevent="changePage(page)"
+        href="#"
+        :style="{ backgroundColor: page === this.page ? 'blue' : '#1a1a1a' }"
+      >
+        {{ page }}
+      </a>
+    </span>
+  </div>
+</template>
+<script>
+export default {
+  props: ["pages", "page"],
+  methods: {
+    changePage(p) {
+      if (p !== this.page) {
+        this.$emit("changePage", p);
+      }
+    },
+  },
+};
+</script>
